c7d94d89d84d93938206652f9cf2649c541ca9be
Ambaaq-Ajibike <ajibikeambaaq@gmail.com>
2024-09-03 18:54:00 +0100

    header responsiveness

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Link from 'next/link';
 import { RoundedButton } from './RoundedButton'
+import { CiMenuBurger } from 'react-icons/ci';
+import { IoMdClose } from 'react-icons/io';
 
 export const Header = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(true);
   return (
-    <div className='flex justify-between items-center px-40 py-10 bg-primary'>
-        <h1 className='text-white text-4xl font-extrabold cursor-pointer'>Matrix<span className='text-secondary'>Momentum</span></h1>
-        <ul className='flex text-white justify-around grow'>
-            <li className='cursor-pointer'><Link href="/">HOME</Link></li>
-            <li className='cursor-pointer'><Link href="/aboutus">ABOUT US</Link></li>
-            <li className='cursor-pointer'><Link href="/faq">FAQ</Link></li>
-            <li className='cursor-pointer'><Link href="/termofuse">TERM OF USE</Link></li>
-            <li className='cursor-pointer'><Link href="/contactus">CONTACT US</Link></li>
+    <div className='flex md:flex-row flex-col justify-between  md:items-center items-start md:px-40 px-10 py-10 bg-primary'>
+      <div className='flex justify-between gap-10'>
+      <h1 className='text-white text-4xl font-extrabold cursor-pointer'>Matrix<span className='text-secondary '>Momentum</span></h1>
+      <CiMenuBurger onClick={() => setShowMenu(!showMenu)} className={`md:hidden ${!showMenu ?  "hidden" : ""} text-secondary text-4xl font-extrabold cursor-pointer`}/>
+        <IoMdClose  onClick={() => setShowMenu(!showMenu)} className={`md:hidden ${showMenu ?  "hidden" : ""} text-secondary text-4xl font-extrabold cursor-pointer`}/>
+      </div>
+        
+       {
+            showMenu &&
+            <>
+               <ul className='flex md:flex-row flex-col  text-white justify-around grow'>
+            <li className='cursor-pointer md:m-0 m-5'><Link href="/">HOME</Link></li>
+            <li className='cursor-pointer md:m-0 m-5'><Link href="/aboutus">ABOUT US</Link></li>
+            <li className='cursor-pointer md:m-0 m-5'><Link href="/faq">FAQ</Link></li>
+            <li className='cursor-pointer md:m-0 m-5'><Link href="/termofuse">TERM OF USE</Link></li>
+            <li className='cursor-pointer md:m-0 m-5'><Link href="/contactus">CONTACT US</Link></li>
         </ul>
-        <ul className='flex'>
-            <li><select className='bg-secondary text-primary rounded-full p-3 mr-2'>
+        <ul className='flex md:flex-row flex-col '>
+            <li><select className='bg-secondary text-primary rounded-full p-3 mr-2 mb-5 md:mb-0'>
                     <option selected value="Yoruba">Select Language</option>
                     <option value="Yoruba">Yoruba</option>
                     <option value="English">English</option>
@@ -24,6 +36,9 @@ export const Header = () => {
                     <RoundedButton bgColor='bg-secondary' color='text-primary' text='LOGIN' />
                 </li>
         </ul>
+            </>
+           
+       }
     </div>
   )
 }
